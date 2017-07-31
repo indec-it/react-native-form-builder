65c9c354c9420d3e14dda5bfb3da2804af06f66a
ricky cambra <ricky.cambra@gmail.com>
2017-07-31 15:47:38 -0300

    feat(yesNoQuestion): add DK button

--- src/questions/YesNoQuestion.js
+++ src/questions/YesNoQuestion.js
@@ -1,43 +1,53 @@
 /* eslint jsx-a11y/label-has-for:"off" */
 import React from 'react';
 import PropTypes from 'prop-types';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import {ButtonGroup} from 'react-native-elements';
 
-const styles = StyleSheet.create({
-    rowContainer: {
-        margin: 8,
-        flexDirection: 'row',
-        justifyContent: 'center',
-    },
-    button: {
-        marginHorizontal: 8,
+const getValue = (index, question) => {
+    switch (index) {
+        case 0:
+            return question.trueValue;
+        case 1:
+            return question.falseValue;
+        case 2:
+            return question.dkValue;
+        default:
+            return null;
     }
-});
+};
+
+const getSelectedValue = (answer, question) => {
+    switch (answer) {
+        case question.trueValue:
+            return 0;
+        case question.falseValue:
+            return 1;
+        case question.dkValue:
+            return 2;
+        default:
+            return null;
+    }
+};
+
+const onPress = (index, callback, question) => {
+    const value = getValue(index, question);
+    return callback({target: {name: question.name, value}})
+};
 
 const YesNoQuestion = ({answer, question, onChange}) => {
-    const change = (index, callback) => {
-        const value = index === 0;
-        return callback({target: {name: question.name, value}})
-    };
-    const component1 = () => <Text>SI</Text>;
-    const component2 = () => <Text>NO</Text>;
-    const buttons = [{element: component1}, {element: component2}];
-    const selected = answer => {
-        if (answer) {
-            return 0;
-        } else if (answer === false) {
-            return 1;
-        }
-        return null;
-    };
+    const buttons = [{element: () => <Text>SI</Text>}, {element: () => <Text>NO</Text>}];
+    if (question.dkValue) {
+        buttons.push({element: () => <Text>{question.dkLabel}</Text>})
+    }
+
     return (
         <View>
             <Text>{question.number ? `${question.number}` : ''}</Text>
             <Text>{question.text}</Text>
             <ButtonGroup
-                onPress={index => change(index, onChange)}
-                selectedIndex={selected(answer)}
+                onPress={index => onPress(index, onChange, question)}
+                selectedIndex={getSelectedValue(answer, question)}
                 buttons={buttons}
                 containerStyle={{height: 100}}
                 selectedBackgroundColor="#41B5FF"
@@ -47,7 +57,7 @@ const YesNoQuestion = ({answer, question, onChange}) => {
 };
 
 YesNoQuestion.propTypes = {
-    answer: PropTypes.bool,
+    answer: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
     onChange: PropTypes.func.isRequired,
     question: PropTypes.shape({}).isRequired
 };
