26992e22eeaf9d575c0097f78449043d9a861d83
Juan López <juan1manuel9lopez96@gmail.com>
2017-10-06 02:09:36 -0300

    fix(total): fix total callback

--- src/questions/Total.js
+++ src/questions/Total.js
@@ -18,6 +18,9 @@ const getTotal = (section, question, callback) => {
     if (isNaN(total)) {
         total = 0;
     }
+
+    if (section[question.name] === total) return total;
+
     callback({[question.name]: total});
     return total;
 };
