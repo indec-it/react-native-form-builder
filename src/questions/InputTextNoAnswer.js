9e4b6a8cca6eb0f5c2f3bf4f815966c15cdae87e
Juan López <juan1manuel9lopez96@gmail.com>
2017-10-10 12:34:05 -0300

    fix(InputNoAnswer): fix callback

--- src/questions/InputTextNoAnswer.js
+++ src/questions/InputTextNoAnswer.js
@@ -4,13 +4,6 @@ import {Text, TextInput, View, Button} from 'react-native';
 
 import styles from './styles';
 
-const handleChange = (value, callback, question) => callback({target: {name: question.name, value}});
-
-const setNoAnswer = (value, callback, question) => {
-    callback({target: {name: question.name, value}});
-    callback({target: {name: `${question.name}NoAnswer`, value: true}});
-};
-
 class InputTextNoAnswer extends Component {
     static propTypes = {
         answer: PropTypes.string,
@@ -27,20 +20,33 @@ class InputTextNoAnswer extends Component {
         this.state = {block: false};
     }
 
+    setNoAnswer() {
+        const {onChange, question} = this.props;
+        const {block} = this.state;
+        onChange({target: {name: `${question.name}NoAnswer`, value: !block}});
+        this.setState({block: !block});
+    }
+
+    handleChange(value) {
+        const {onChange, question} = this.props;
+        onChange({target: {name: question.name, value}});
+    }
+
     render() {
-        const {question, answer, onChange} = this.props;
+        const {question, answer} = this.props;
+        const {block} = this.state;
         return (
             <View style={styles.rowContainer}>
                 <Text>{question.number ? `${question.number}` : ''}</Text>
                 <Text>{question.text}</Text>
-                {!this.state.block && <TextInput
+                {!block && <TextInput
                     value={answer}
-                    onChangeText={text => handleChange(text, onChange, question)}
+                    onChangeText={text => this.handleChange(text)}
                 />}
-                {this.state.block && <Text style={{color: 'grey'}}>{answer}</Text>}
+                {block && <Text style={{color: 'grey'}}>(Sin Nombre)</Text>}
                 <Button
                     title="S/N"
-                    onPress={() => setNoAnswer(question.disableValue, onChange, question)}
+                    onPress={() => this.setNoAnswer()}
                 />
             </View>
         );
