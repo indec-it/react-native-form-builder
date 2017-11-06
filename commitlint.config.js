8fe9e33694f55cadd11939156b4c49752dd1fcd3
Axel Navarro <navarroaxel@gmail.com>
2017-11-06 18:17:00 -0300

    fix(packages): update eslint and md-textinput

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,10 +6,11 @@ module.exports = {
         'footer-tense': [1, 'always', ['present-imperative']],
         'header-max-length': [2, 'always', 72],
         lang: [1, 'always', 'eng'],
+        'scope-case': [2, 'always', 'camel-case'],
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
         'subject-tense': [1, 'always', ['present-imperative']],
-        'type-case': [2, 'always', 'lowerCase'],
+        'type-case': [2, 'always', 'lower-case'],
         'type-empty': [2, 'never'],
         'type-enum': [
             2,
