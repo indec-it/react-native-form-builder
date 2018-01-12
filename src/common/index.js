be05763d97463d314b77e25b50a4a5c3fa00a2aa
juanlopez1 <juan1manuel9lopez96@gmail.com>
2018-01-12 11:22:19 -0300

    feat(components): remove common and add RNC

--- src/common/index.js
+++
@@ -1,7 +0,0 @@
-import Col from './Col';
-import Grid from './Grid';
-import Row from './Row';
-
-export {Col};
-export {Grid};
-export {Row};
--- src/components/RadioTable/index.js
+++ src/components/RadioTable/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Text, View} from 'react-native';
 import {CheckBox} from 'react-native-elements';
-import {Grid, Row, Col} from '../../common';
+import {Row, Col, Grid} from '@indec/react-native-commons';
 
 import TextWithBadge from '../TextWithBadge';
 import styles from './styles';
@@ -37,14 +37,15 @@ const RadioTable = ({section, question, onChange}) => (
         <TextWithBadge question={question}/>
         <Grid>
             <Row>
-                <Col size={4}/>
-                {question.options.map(option => (
-                    <Col style={styles.column}>
-                        <Text>
-                            {option.text}
-                        </Text>
-                    </Col>
-                ))}
+                <Col size={4}>
+                    {question.options.map(option => (
+                        <Col style={styles.column}>
+                            <Text>
+                                {option.text}
+                            </Text>
+                        </Col>
+                    ))}
+                </Col>
             </Row>
             {question.questions.map(questionRow =>
                 renderRow(section, question.options, question.name, questionRow, onChange)
