8d3bd5e1df2e9a485c3de954abebdf88731e5a7d
lukebell <lukebell@arwen.local>
2018-02-09 20:02:42 -0300

    feat(formgroup): add FormGroup and Tabs components

---
+++ src/components/Tabs/styles.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        marginTop: 8
+    },
+    tab: {
+        backgroundColor: '#e4e4e4',
+        padding: 16,
+        marginRight: 8,
+        borderColor: '#cecece',
+        borderWidth: 0.5,
+        marginTop: 8
+    },
+    selected: {
+        backgroundColor: '#0088ff',
+        color: 'white',
+        marginTop: 0
+    },
+    footer: {
+        backgroundColor: '#cecece',
+        height: 1
+    }
+});
