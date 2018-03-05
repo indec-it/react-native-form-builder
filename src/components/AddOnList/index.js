6eb9c51110d27caaf9ae6847f6c45ea6be33d4f1
Matias Dumrauf <matias.dumrauf@gmail.com>
2018-03-05 01:47:28 -0300

    fix(addOnList): typo in text

--- src/components/AddOnList/index.js
+++ src/components/AddOnList/index.js
@@ -119,7 +119,7 @@ export default class AddOnList extends Component {
     deleteRow(index) {
         Alert.alert(
             'Eliminar',
-            '¿Desea eliminar esta declaracion?',
+            '¿Desea eliminar esta declaración?',
             [{
                 text: 'Cancelar'
             }, {
