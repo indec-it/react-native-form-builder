64833960f406a3bc488951e5243ccf377af4efae
Pablo Bastidas Carbonell <p.bastidascarbonell@gmail.com>
2018-03-06 17:24:33 -0300

    style(commonStyles): stylized queries opacity

--- src/components/commonStyles.js
+++ src/components/commonStyles.js
@@ -3,6 +3,8 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
     disabledContainer: {
         opacity: 0.3,
+        marginLeft: 8,
+        marginTop: 9,
         flex: 1
     }
 });
