05d861707fbee36bb555da4cb322077b0a5cec37
juanlopez1 <juan1manuel9lopez96@gmail.com>
2018-03-06 08:30:28 -0300

    feat(multiSelect): add MultiSelect component

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,16 @@
 module.exports = {
     rules: {
         'body-leading-blank': [1, 'always'],
-        'body-tense': [1, 'always', ['present-imperative']],
         'footer-leading-blank': [1, 'always'],
-        'footer-tense': [1, 'always', ['present-imperative']],
         'header-max-length': [2, 'always', 72],
-        lang: [1, 'always', 'eng'],
         'scope-case': [2, 'always', 'camel-case'],
+        'subject-case': [
+            2,
+            'never',
+            ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+        ],
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
-        'subject-tense': [1, 'always', ['present-imperative']],
         'type-case': [2, 'always', 'lower-case'],
         'type-empty': [2, 'never'],
         'type-enum': [
@@ -17,7 +18,6 @@ module.exports = {
             'always',
             [
                 'build',
-                'chore',
                 'ci',
                 'docs',
                 'feat',
