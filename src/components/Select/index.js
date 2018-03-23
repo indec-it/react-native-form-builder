f470e3a04dfa53de82d1ca530d797bc000424662
Pablo Bastidas Carbonell <p.bastidascarbonell@gmail.com>
2018-03-23 15:10:06 -0300

    style(select): add disabled style to Select component

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -6,11 +6,12 @@ import {mergeStyles, stylePropType} from '@indec/react-native-commons/util';
 import {TextWithBadge} from '..';
 import {handleChange} from '../../util';
 import styles from './styles';
+import commonStyles from '../commonStyles';
 
-const Select = ({answer, question, onChange, style, textWithBadgeStyle}) => {
+const Select = ({answer, question, onChange, style, textWithBadgeStyle, disabled}) => {
     const computedStyles = mergeStyles(styles, style);
     return (
-        <View style={computedStyles.component.container}>
+        <View style={disabled ? commonStyles.disabledContainer : computedStyles.component.container}>
             {question.text && <TextWithBadge
                 question={question}
                 style={textWithBadgeStyle}
@@ -39,13 +40,15 @@ Select.propTypes = {
     onChange: PropTypes.func.isRequired,
     answer: PropTypes.number,
     style: stylePropType,
-    textWithBadgeStyle: stylePropType
+    textWithBadgeStyle: stylePropType,
+    disabled: PropTypes.bool
 };
 
 Select.defaultProps = {
     answer: null,
     style: null,
-    textWithBadgeStyle: null
+    textWithBadgeStyle: null,
+    disabled: false
 };
 
 export default Select;
