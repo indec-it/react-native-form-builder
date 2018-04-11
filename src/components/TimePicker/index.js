dfe9615ceed09104da6f6adfd8389884a9d4ac92
maximilianoforlenza <forlenzamaximiliano@gmail.com>
2018-04-11 11:08:08 -0300

    feat(timePicker): add TimePicker component (#57)

---
+++ src/components/TimePicker/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {View} from 'react-native';
+import DatePicker from 'react-native-datepicker';
+import {mergeStyles, stylePropType} from '@indec/react-native-commons/util';
+
+import {TextWithBadge} from '..';
+import {handleChange} from '../../util';
+import commonStyles from '../commonStyles';
+import styles from './styles';
+
+const TimePicker = ({answer, question, onChange, style, textWithBadgeStyle, disabled}) => {
+    const computedStyles = mergeStyles(styles, style);
+    return (
+        <View style={disabled ? commonStyles.disabledContainer : computedStyles.container}>
+            {question.text && <TextWithBadge
+                question={question}
+                style={textWithBadgeStyle}
+            />}
+            <DatePicker
+                style={computedStyles.datePicker}
+                date={answer}
+                placeholder={question.placeholder}
+                confirmBtnText="Confirmar"
+                cancelBtnText="Cancelar"
+                onDateChange={date => handleChange(question.name, date, onChange)}
+                disabled={disabled}
+                mode="time"
+            />
+        </View>
+    );
+};
+
+TimePicker.displayName = 'timePicker';
+
+TimePicker.propTypes = {
+    question: PropTypes.shape({}).isRequired,
+    onChange: PropTypes.func.isRequired,
+    answer: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string
+    ]),
+    style: stylePropType,
+    textWithBadgeStyle: stylePropType,
+    disabled: PropTypes.bool
+};
+
+TimePicker.defaultProps = {
+    answer: null,
+    style: null,
+    textWithBadgeStyle: null,
+    disabled: false
+};
+
+export default TimePicker;
