17afd7764ca5442577ae2277b953ca5ca33e9e6a
Pablo Bastidas Carbonell <p.bastidascarbonell@gmail.com>
2018-11-01 14:39:16 -0300

    style(select): style borders for Select component

--- src/components/Select/styles.js
+++ src/components/Select/styles.js
@@ -7,9 +7,13 @@ export default {
             flex: 1,
             flexDirection: 'column',
             paddingBottom: 0,
-            paddingTop: 0
+            paddingTop: 0,
+            borderBottomWidth: 1,
+            borderBottomColor: '#d9d9d9',
+            borderRightWidth: 1,
+            borderRightColor: '#d9d9d9',
+            borderBottomRightRadius: 12
         }
     }),
     picker: {}
 };
-
