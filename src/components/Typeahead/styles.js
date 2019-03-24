ccf728f7b768031aff31e490b9f16eedcfcf4d5e
Sergio Cuba <sergiocuba@gmail.com>
2019-03-24 15:20:34 -0300

    feat(src): add autocomplete component

---
+++ src/components/Typeahead/styles.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    suggestionsWrapper: {
+        marginTop: 5,
+        marginBottom: 5
+    },
+    suggestion: {
+        height: 30,
+        padding: 5,
+        borderBottomColor: '#ddd',
+        borderBottomWidth: 1
+    },
+    suggestionText: {
+        fontSize: 15
+    },
+    input: {
+        fontSize: 15
+    },
+    wrapper: {
+        flex: 1
+    }
+});
