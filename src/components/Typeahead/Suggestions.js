a8cb7c5122035983746775e5c4134ff96c4c6f1f
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2019-07-31 22:50:29 -0300

    refactor(typeahead): reduce and simplify component and logic

---
+++ src/components/Typeahead/Suggestions.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Text, TouchableOpacity, View} from 'react-native';
+
+import styles from './styles';
+
+const Suggestions = ({suggestions, onChangeSuggestion}) => (
+    <View style={styles.suggestionsWrapper}>
+        {suggestions.map(suggestion => (
+            <TouchableOpacity
+                suggestionText={suggestion}
+                activeOpacity={0.6}
+                style={styles.suggestion}
+                onPress={onChangeSuggestion(suggestion)}
+            >
+                <View style={styles.wrapper}>
+                    <Text style={styles.suggestionText}>
+                        {suggestion.label}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        ))}
+    </View>
+);
+
+Suggestions.propTypes = {
+    onChangeSuggestion: PropTypes.func.isRequired,
+    suggestions: PropTypes.arrayOf(
+        PropTypes.shape({
+            label: PropTypes.string,
+            value: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+        })
+    ).isRequired
+};
+
+export default Suggestions;
